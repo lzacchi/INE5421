121816ddc24634537a842c4a16195a1fa494e1dd
lzacchi <zacchilucasm@gmail.com>
2021-02-21 21:10:30 -0300

    😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔😔

---
+++ regexp.js
@@ -0,0 +1,5 @@
+function runRegExp() {
+    regexp = document.querySelector('#regexp-custom').value;
+    regexpInput = document.querySelector('#regexp-input').value;
+    document.querySelector('#regexp-result').textContent = "essa coisa: " + regexp + "\n com essa: " + regexpInput + "\n faz algo mas não sei o que é";
+}

21a185ca1aa5c70367f79499e65b4d3aa9a02075
LucasVerdade <lucas.verdade@grad.ufsc.br>
2021-02-22 11:19:00 -0300

    cleaning code

--- regexp.js
+++ regexp.js
@@ -1,5 +1,5 @@
 function runRegExp() {
     regexp = document.querySelector('#regexp-custom').value;
     regexpInput = document.querySelector('#regexp-input').value;
-    document.querySelector('#regexp-result').textContent = "essa coisa: " + regexp + "\n com essa: " + regexpInput + "\n faz algo mas não sei o que é";
+    document.querySelector('#regexp-result').textContent = "RegExp de entrada: " + regexp + "\n String de entrada: " + regexpInput + "\n Saída:";
 }
